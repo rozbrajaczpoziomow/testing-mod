b91f50f7abcb7785956e9b0430b73444856dc907
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2023-11-21 16:46:14 +0100

    Potions! :D

--- src/main/java/mod/rozbrajaczpoziomow/testing/a_registers/EffectRegister.java
+++ src/main/java/mod/rozbrajaczpoziomow/testing/a_registers/EffectRegister.java
@@ -3,10 +3,14 @@ package mod.rozbrajaczpoziomow.testing.a_registers;
 import mod.rozbrajaczpoziomow.testing.effects.ShuffleInventoryEffect;
 import mod.rozbrajaczpoziomow.testing.effects.StripperEffect;
 import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Potion;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
 
 import static mod.rozbrajaczpoziomow.testing.TestingMod.MOD_ID;
 
@@ -16,11 +20,23 @@ public class EffectRegister {
 	public static final RegistryObject<ShuffleInventoryEffect> ShuffleInventoryEffect = r("shuffle_inventory", new ShuffleInventoryEffect());
 	public static final RegistryObject<StripperEffect> StripperEffect = r("stripper", new StripperEffect()); // TODO: recipe - mundane potion + yeetr x lol
 
+
+
+	public static final DeferredRegister<Potion> PotionConstruct = DeferredRegister.create(ForgeRegistries.POTION_TYPES, MOD_ID);
+
+	public static final RegistryObject<Potion> ShuffleInventoryPotion = p("shuffle_inventory", () -> new Potion(new EffectInstance(ShuffleInventoryEffect.get(), 20 * 60)));
+	public static final RegistryObject<Potion> StripperPotion = p("stripper", () -> new Potion(new EffectInstance(StripperEffect.get(), 20 * 60)));
+
 	public static <T extends Effect> RegistryObject<T> r(String n, T effect) {
 		return EffectConstruct.register(n, () -> effect);
 	}
 
+	public static <T extends Potion> RegistryObject<T> p(String n, Supplier<T> potion) {
+		return PotionConstruct.register(n, potion);
+	}
+
 	public static void register(IEventBus eventBus) {
 		EffectConstruct.register(eventBus);
+		PotionConstruct.register(eventBus);
 	}
 }
