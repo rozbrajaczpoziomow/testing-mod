91bfd2e6cfc100a8b0e679ffd2b929254eb0463f
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2023-11-21 12:40:56 +0100

    Glowdust refactor

--- src/main/java/mod/rozbrajaczpoziomow/testing/items/Glowdust.java
+++ src/main/java/mod/rozbrajaczpoziomow/testing/items/Glowdust.java
@@ -2,7 +2,6 @@ package mod.rozbrajaczpoziomow.testing.items;
 
 import com.google.common.collect.ImmutableList;
 import mod.rozbrajaczpoziomow.testing.a_registers.BlockRegister;
-import mod.rozbrajaczpoziomow.testing.blocks.BaseIBABlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -16,20 +15,29 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static mod.rozbrajaczpoziomow.testing.Utils.text;
 import static mod.rozbrajaczpoziomow.testing.a_registers.TagRegister.TagIBAGlowdustableBlocks;
+import static mod.rozbrajaczpoziomow.testing.blocks.BaseIBABlock.lightLevel;
 import static net.minecraft.util.text.TextFormatting.GOLD;
 
 public class Glowdust extends Item {
+	private static final HashMap<Block, Block> specialBlockTransformations = new HashMap<>();
 	public Glowdust(Properties properties) {
 		super(properties);
+	}
+
+	static {
+		specialBlockTransformations.put(Blocks.RED_NETHER_BRICKS, BlockRegister.GlowingRedNetherBricks.get());
+		specialBlockTransformations.put(Blocks.STONE_BRICKS, BlockRegister.GlowingStoneBricks.get());
 	}
 
 	// ! Doesn't automatically update, code doesn't use it, just for HelpBook.
@@ -43,38 +51,28 @@ public class Glowdust extends Item {
 	@Override
 	public ActionResultType useOn(ItemUseContext ctx) {
 		BlockState block = ctx.getLevel().getBlockState(ctx.getClickedPos());
-		Hand hand = ctx.getHand();
 
-		// TODO: this is absolutely terrible code
-		if(hand == Hand.MAIN_HAND) {
-			if(block.is(TagIBAGlowdustableBlocks))
-				if(block.getValue(BaseIBABlock.lightLevel) == 0)
-					ctx.getLevel().setBlock(ctx.getClickedPos(), block.setValue(BaseIBABlock.lightLevel, 15), 2 | 16 | 32);
-				else return ActionResultType.FAIL;
-			else if (block.getBlock() == Blocks.RED_NETHER_BRICKS)
-				ctx.getLevel().setBlock(ctx.getClickedPos(), BlockRegister.GlowingRedNetherBricks.get().defaultBlockState(), 2 | 16 | 32);
-			else if (block.getBlock() == Blocks.STONE_BRICKS)
-				ctx.getLevel().setBlock(ctx.getClickedPos(), BlockRegister.GlowingStoneBricks.get().defaultBlockState(), 2 | 16 | 32);
-			else
-				return ActionResultType.FAIL;
+		boolean light = ctx.getHand() == Hand.MAIN_HAND;
+		if(!enlighten(ctx.getLevel(), ctx.getClickedPos(), block, light))
+			return ActionResultType.FAIL;
 
-			ctx.getItemInHand().setCount(ctx.getItemInHand().getCount() - 1);
-		} else {
-			if(block.is(TagIBAGlowdustableBlocks))
-				if(block.getValue(BaseIBABlock.lightLevel) == 15)
-					ctx.getLevel().setBlock(ctx.getClickedPos(), block.setValue(BaseIBABlock.lightLevel, 0), 2 | 16 | 32);
-				else return ActionResultType.FAIL;
-			else if(block.getBlock() == BlockRegister.GlowingRedNetherBricks.get())
-				ctx.getLevel().setBlock(ctx.getClickedPos(), Blocks.RED_NETHER_BRICKS.defaultBlockState(), 2 | 16 | 32);
-			else if(block.getBlock() == BlockRegister.GlowingStoneBricks.get())
-				ctx.getLevel().setBlock(ctx.getClickedPos(), Blocks.STONE_BRICKS.defaultBlockState(), 2 | 16 | 32);
-			else
-				return ActionResultType.FAIL;
+		ctx.getItemInHand().setCount(ctx.getItemInHand().getCount() + (light? -1 : 1));
+		return ActionResultType.SUCCESS;
+	}
 
-			ctx.getItemInHand().setCount(ctx.getItemInHand().getCount() + 1);
+	private boolean enlighten(World world, BlockPos pos, BlockState state, boolean light) {
+		if(state.is(TagIBAGlowdustableBlocks)) {
+			int bef = state.getValue(lightLevel);
+			world.setBlock(pos, state.setValue(lightLevel, light? 15 : 0), 2 | 16 | 32);
+			return bef == (light? 0 : 15);
+		} else if(light && specialBlockTransformations.containsKey(state.getBlock())) {
+			return world.setBlock(pos, specialBlockTransformations.get(state.getBlock()).defaultBlockState(), 2 | 16 | 32);
+		} else if(!light && specialBlockTransformations.containsValue(state.getBlock())) {
+			//noinspection OptionalGetWithoutIsPresent - will always be present
+			return world.setBlock(pos, specialBlockTransformations.entrySet().stream().filter(ent -> ent.getValue() == state.getBlock()).findAny().get().getKey().defaultBlockState(), 2 | 16 | 32);
 		}
 
-		return ActionResultType.SUCCESS;
+		return false;
 	}
 
 	@Override
