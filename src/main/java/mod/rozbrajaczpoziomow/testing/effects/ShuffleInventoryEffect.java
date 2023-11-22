b68d796d58145b04b5cdf2ae7532f96f24fe854e
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2023-11-22 13:41:08 +0100

    Shuffle Inventory rework

--- src/main/java/mod/rozbrajaczpoziomow/testing/effects/ShuffleInventoryEffect.java
+++ src/main/java/mod/rozbrajaczpoziomow/testing/effects/ShuffleInventoryEffect.java
@@ -1,6 +1,5 @@
 package mod.rozbrajaczpoziomow.testing.effects;
 
-import com.google.common.collect.ImmutableList;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -9,10 +8,9 @@ import net.minecraft.potion.EffectType;
 import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.extensions.IForgeEffect;
 
-import java.util.stream.Collectors;
+import java.util.Random;
 
 import static mod.rozbrajaczpoziomow.testing.Utils.rgba;
-import static mod.rozbrajaczpoziomow.testing.Utils.rng;
 
 public class ShuffleInventoryEffect extends Effect implements IForgeEffect {
 	public ShuffleInventoryEffect() {
@@ -26,23 +24,27 @@ public class ShuffleInventoryEffect extends Effect implements IForgeEffect {
 
 	@Override
 	public void applyEffectTick(LivingEntity livingEntity, int amplifier) {
+		if(livingEntity.getCommandSenderWorld().isClientSide)
+			return;
+
 		if(!(livingEntity instanceof PlayerEntity))
 			return;
 
 		PlayerEntity player = (PlayerEntity) livingEntity;
 		NonNullList<ItemStack> inv = player.inventory.items;
-		ImmutableList<ItemStack> items = ImmutableList.copyOf(inv.stream().filter(stack -> stack != ItemStack.EMPTY).collect(Collectors.toList())); // why
-		inv.clear();
-		for(ItemStack stack : items) {
-			while(true) {
-				int slot = rng(36);
-
-				if(inv.get(slot) != ItemStack.EMPTY)
-					continue;
-
-				inv.set(slot, stack);
-				break;
-			}
+		Random rand = new Random(player.tickCount);
+		int c = 2; // happens c-1 times
+		while(--c != 0) {
+			int from = rand.nextInt(inv.size());
+			int to = rand.nextInt(inv.size());
+			ItemStack tmp = inv.get(to);
+			inv.set(to, inv.get(from));
+			inv.set(from, tmp);
 		}
+//		ItemStack last = inv.get(inv.size() - 1);
+//		int i = inv.size() - 1;
+//		while(i != 0)
+//			inv.set(i--, inv.get(i));
+//		inv.set(0, last);
 	}
 }
